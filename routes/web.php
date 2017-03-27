dc2ba98ec2781abbd3371cb30bb048c7b552da91
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2017-03-27 17:31:17 -0300

    Mudado alguns métodos de request

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ Route::name('home')->get('/', function () { return view('welcome'); });
 # TURNOS
 Route::name('turnos')->get('turnos', 'TurnoController@index');
 Route::name('turno.cadastrar')->get('turno/cadastrar', 'TurnoController@cadastrar');
-Route::name('turno.salvar')->get('turno/salvar', 'TurnoController@salvar');
+Route::name('turno.salvar')->post('turno/salvar', 'TurnoController@salvar');
 Route::name('turno.editar')->get('turno/editar/{id}', 'TurnoController@editar');
-Route::name('turno.atualizar')->get('turno/atualizar', 'TurnoController@atualizar');
+Route::name('turno.atualizar')->patch('turno/atualizar', 'TurnoController@atualizar');
 
 # DISCIPLINAS
 Route::name('disciplinas')->get("disciplinas", 'DisciplinaController@index');
