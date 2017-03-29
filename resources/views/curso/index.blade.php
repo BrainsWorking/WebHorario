ec3919c0f33750859f42c391fe872666b16d3edb
Alex <asasouza.alex@gmail.com>
2017-03-29 19:32:55 -0300

    Modificações Index.

--- resources/views/curso/index.blade.php
+++ resources/views/curso/index.blade.php
@@ -1,20 +1,25 @@
 @extends('layout.principal')
 
-	@section('title', 'Cursos')
+@section('title', 'Cursos')
 
-	@section('content')
-	@parent
-	<div class="col-lg-12 table-responsive">
-		<h1 class="text-center page-header"></h1>
-		<table class="table table-condensed table-hover">
-			<thead>
-				<a class="btn btn-success btn-lg right" href="{{ route('curso.cadastrar') }}"><span class="glyphicon glyphicon-plus"></span> Cadastrar</a>
+@section('content')
+@parent
+<div class="col-lg-12 table-responsive">
+	<h1 class="text-center page-header"></h1>
+	<table class="table table-condensed table-hover">
+		<thead>
+			<div class="padding-right-0 padding-left-0 top-bar">
+				<div class="col-sm-8 padding-left-0">
+					<div class="input-group col-lg-12">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+						<input type="text" class="form-control input-filter" placeholder="Pesquisar">
+					</div>
+				</div>
 
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 barra-pesquisa">
-						<div class="input-group">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
-							<input type="text" class="form-control input-filter" placeholder="Pesquisar">
-						</div>
+				<div class="col-sm-4 padding-right-0">
+					<a class="btn btn-success right col-sm-12" href="{{ route('curso.cadastrar') }}"><span
+						class="glyphicon glyphicon-plus"></span> Cadastrar</a>
+					</div>
 				</div>
 
 				<th class="text-center"></th>
@@ -33,7 +38,7 @@
 					<td class="text-center search"> {{$curso->nome}} </td>
 					<td class="text-center search"> {{$curso->iniciais}} </td>
 					<td class="text-center"><a href="{{ route('curso.editar', $curso->id) }}"><span class="glyphicon glyphicon-edit"></span></a></td>
-					<td class="text-center"><a href=""><span class="glyphicon glyphicon-remove"></span></a></td>
+					<td class="text-center"><a href="" class="table-delete"><span class="glyphicon glyphicon-remove"></span></a></td>
 				</tr>
 				<tr class="hidden-info">
 					<td colspan="5">
@@ -43,7 +48,7 @@
 						<p><b>Disciplinas:</b></p>
 
 						@foreach($curso->disciplinas as $disciplina)
-							<span class="col-lg-3 text-center">{{ $disciplina['iniciais'] .' - '. $disciplina['nome'] }}</span>
+						<span class="col-lg-3 text-center">{{ $disciplina['iniciais'] .' - '. $disciplina['nome'] }}</span>
 						@endforeach
 					</td>
 				</tr>
