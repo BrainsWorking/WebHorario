66dd7a3eba68de55e8ffcf0271b7f207f5375dde
Pliavi <pliavi.rpgm@gmail.com>
2017-04-14 18:41:45 -0300

    Pequenas alterações

--- app/Helpers/Formatters.php
+++ app/Helpers/Formatters.php
@@ -30,3 +30,20 @@ function formataRG($rg){
     $rg = substr_replace($rg,'-', -1, 0);
     return $rg;
 }
+
+function formataTelefone($telefone){
+    $telSize = strlen($telefone);
+
+    if($telSize == 11) {
+        $telefone = substr_replace($telefone,  '(',  0, 0);
+        $telefone = substr_replace($telefone, ') ',  3, 0);
+        $telefone = substr_replace($telefone,  '-',  6, 0);
+        $telefone = substr_replace($telefone,  '-', 11, 0);
+    } elseif ($telSize == 10) {
+        $telefone = substr_replace($telefone,  '(',  0, 0);
+        $telefone = substr_replace($telefone, ') ',  3, 0);
+        $telefone = substr_replace($telefone,  '-', 9, 0);
+    } 
+    
+    return $telefone;
+}
--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -140,11 +140,25 @@
     <script type="text/javascript" src="{{ asset('/js/bootbox.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/js/off-canvas.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('/js/footer.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.mask.js') }}"></script>
 
     @yield('scripts')
     <script>
         $('.mascara-data').mask('00/00/0000');
         $('.mascara-rg').mask('00.000.000-0');
+        $('.mascara-cpf').mask('000.000.000-00');
+        
+        var SPMaskBehavior = function (val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 0-0000-0000' : '(00) 0000-00009';
+        }
+
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+
+        $('.mascara-telefone').mask(SPMaskBehavior, spOptions);
     </script>
 </body>
 
