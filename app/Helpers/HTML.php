89dfacab8668cbf76c0bdef8170b791db2a364a3
Pliavi <pliavi.rpgm@gmail.com>
2017-04-14 14:16:58 -0300

    adicionado funcoes de convesão de data EN e formatação de RG

--- app/Helpers/HTML.php
+++ app/Helpers/HTML.php
@@ -10,10 +10,23 @@ function converterDataBrasil($data){
 	return $dataFormatada[2] . "/". $dataFormatada[1] ."/". $dataFormatada[0];
 }
 
+function converterDataIngles($data){
+    $dataFormatada = explode('/', $data);
+
+	return $dataFormatada[2] . "-". $dataFormatada[1] ."-". $dataFormatada[0];
+}
+
 function limpaPontuacao($valor) {
     return preg_replace('/[^\d]/', '', $valor);
 }
 
 function formataCPF($cpf){
     return substr_replace(wordwrap($cpf, 3, '.', true), '-', -3, 1);
+}
+
+function formataRG($rg){
+    $rg = substr_replace($rg,'.', -7, 0);
+    $rg = substr_replace($rg,'.', -4, 0);
+    $rg = substr_replace($rg,'-', -1, 0);
+    return $rg;
 }
