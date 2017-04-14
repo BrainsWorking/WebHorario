ca952dd189f0f89bf0ee470332fee82f49f7364f
Alex <asasouza.alex@gmail.com>
2017-04-14 17:04:50 -0300

    Tamanho do link

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -19,13 +19,13 @@
     <div class="container-fluid">
         <div class="row">
             <header class="col-xs-12 col-sm-12">
-                <div class="col-xs-6 col-sm-6">
+                <div class="col-xs-6 col-sm-3">
                     <a href="{{ route('home') }}">
                         <img class="img-responsive webhorario-logo pull-left" src="{{ asset('/img/webhorario.png') }}">
                         <h3 class="hidden-xs">WebHorário</h3>
                     </a>
                 </div>
-                <div class="col-xs-6 col-sm-6">
+                <div class="col-xs-6 col-sm-6 right">
                     <a href="http://www.ifspcaraguatatuba.edu.br" target="_blank"><img class="img-responsive ifsp-logo pull-right" src="{{ asset('/img/ifsp.png') }}"></a>
                 </div>
             </header>
