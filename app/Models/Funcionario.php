af9cae3f5915dbce05d0b720742f900e5b08ca14
Pliavi <pliavi.rpgm@gmail.com>
2017-04-14 14:18:25 -0300

    Corrigidos todos os getters e setters e adiconado softdeletes

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -6,19 +6,57 @@ use App\Models\Cargo;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Traits\UsuarioTrait as Permissivel;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Funcionario extends Authenticatable {
     use Notifiable;
     use Permissivel;
+    use SoftDeletes;
 
-    protected $fillable = ['nome', 'prontuario', 'sexo', 'cpf', 'data_nascimento', 'endereco', 'foto', 'email', 'password', 'cargo_id'];
+    protected $fillable = ['nome', 'prontuario', 'rg', 'sexo', 'cpf', 'data_nascimento', 'endereco', 'foto', 'email', 'password'];
     protected $hidden = [ 'password', 'remember_token' ];
 	public $timestamps = false;
+    protected $dates = ['deleted_at'];
 	
     public function cargos(){
         return $this->belongsToMany(Cargo::class, 'cargos_funcionarios');
     }
 
+    # Retorna o curso no qual é coordenador
+    public function curso(){
+        return $this->hasOne(Curso::class);
+    }
+
+    public function isCoordenador(){
+        return !is_null($this->curso);
+    }
+
+    # Retorna uma string formatada listando os cargos
+    public function getListaCargosAttribute() {
+        $cargos = '';
+        $allCargos = $this->cargos;
+
+        if($this->isCoordenador()) {
+            $allCargos['coordenador'] = new Cargo(['id' => 0, 'nome' => 'Coordenador de ' . $this->curso->sigla ]);
+        }
+
+        $nCargos = count($allCargos);
+        $i = 1;
+        foreach($allCargos as $cargo) {
+            if($i == $nCargos && $nCargos > 1) {
+                $cargos .= " e {$cargo->nome}";
+            } elseif($nCargos == 1 || $i == $nCargos-1) {
+                $cargos .= "{$cargo->nome}";
+            } else {
+                $cargos .= "{$cargo->nome}, ";
+            }
+            $i++;
+        }
+
+        return $cargos;
+    }
+
+    # Retorna o sexo "por extenso"
     public function getSexoAttribute(){ 
         $sexo = strtolower($this->attributes['sexo']);
         if($sexo == 'f') {
@@ -32,13 +70,33 @@ class Funcionario extends Authenticatable {
         return $sexo;
     }
 
-    public function setCpfAttribute($value){
-        $this->attributes['cpf'] = limpaPontuacao($value);
+    # Retorna o sexo no formato padrão para uso no formulário do Collective
+    public function formSexoAttribute(){
+        return $this->attributes['sexo'];
+    }
+
+    public function setDataNascimentoAttribute($data){
+        $this->attributes['data_nascimento'] = converterDataIngles($data);
+    }
+
+    public function getDataNascimentoAttribute(){
+        return converterDataBrasil($this->attributes['data_nascimento']);
+    }
+
+    public function setRgAttribute($rg){
+        $this->attributes['rg'] = limpaPontuacao($rg);
+    }
+
+    public function getRgAttribute(){
+        return formataRG($this->attributes['rg']);
+    }
+
+    public function setCpfAttribute($cpf){
+        $this->attributes['cpf'] = limpaPontuacao($cpf);
     }
 
     public function getCpfAttribute(){
         return formataCPF($this->attributes['cpf']);
     }
 
-    public function setRememberToken($value){} // FIXIT: Só para não dar erro de falta de remember_token
 }
