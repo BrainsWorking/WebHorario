f8eeb3cbb70ebb9738326e7771f614bc291a24f9
VITOR <VITOR>
2017-04-19 21:51:03 -0300

    Sincronizar e Semestre atualizado - Removido problema do sincronizar que dava revert - Atualizado Semestre, datas e valor ativo

--- app/Models/Semestre.php
+++ app/Models/Semestre.php
@@ -10,7 +10,32 @@ class Semestre extends Model
 	
     protected $fillable = ['nome', 'inicio', 'fim'];
 
-    public function disciplinas(){
+    public function disciplinas() {
         return $this->belongsToMany(Disciplina::class, 'disciplinas_semestres');
+    }
+
+    public function setInicioAttribute($data) {
+        $this->attributes['inicio'] = converterDataBrasi($data);
+    }
+
+    public function getInicioAttribute() {
+        return converterDataIngles($this->attributes['inicio']);
+    }
+
+    public function setFimAttribute($data) {
+        $this->attributes['fim'] = converterDataBrasi($data);
+    }
+
+    public function getFimAttribute() {
+        return converterDataIngles($this->attributes['fim']);
+    }
+
+    public function getAtivoAttribute() {
+        $dataAtual = date('Y-m-d');
+        $inicio    = $this->attributes['inicio'];
+        $fim       = $this->attributes['fim'];
+
+        return ($inicio < $dataAtual && $dataAtual < $fim);
+
     }
 }
