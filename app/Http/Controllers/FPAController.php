b6c8fec52b0e97c8d6033e9e2752b29a18ed8e37
LUCAS <LUCAS>
2017-04-19 21:58:17 -0300

    Inicio da construção do controller de FPA

---
+++ app/Http/Controllers/FPAController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Instituicao;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FPAController extends Controller{
+    
+    public function index(){
+
+    }
+
+    public function salvar(){
+
+    }
+
+    public function cadastrar(){
+        return view('Fpa.formFpa');
+    }
+
+    public function editar(){
+
+    }
+
+    public function atualizar(){
+
+    }
+
+    public function deletar(){
+
+    }
+
+}
+
+?>
