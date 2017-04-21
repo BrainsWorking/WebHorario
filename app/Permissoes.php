e80de8ee083c55f1d5c40e6df5151e97af70bc60
Pliavi <pliavi.rpgm@gmail.com>
2017-04-21 01:56:42 -0300

    adicionando todos os cargos

--- app/Permissoes.php
+++ app/Permissoes.php
@@ -2,7 +2,8 @@
 namespace App;
 
 class Permissoes {
-
+    private function secretaria() { return []; }
+    private function diretor_educacional(){ return []; }
+    private function coordenador(){ return []; }
     private function professor(){ return []; }
-
 }
