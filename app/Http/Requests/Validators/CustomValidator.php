2244e54415c95bedeae714862229c0f88ecedd78
Pliavi <pliavi.rpgm@gmail.com>
2017-04-23 12:52:02 -0300

    removido timeAfter e timeBefore agora funciona

--- app/Http/Requests/Validators/CustomValidator.php
+++ app/Http/Requests/Validators/CustomValidator.php
@@ -1,29 +1,48 @@
 <?php
-namespace Requests\Validators;
+namespace App\Http\Requests\Validators;
 
 use Illuminate\Validation\Validator;
 use Illuminate\Support\Arr;
+use Illuminate\Validation\Concerns\ValidatesAttributes;
+use Illuminate\Validation\Concerns\ReplacesAttributes;
+use InvalidArgumentException;
 
-class CustomValidator extends Validator {
+class CustomValidator {
 
-    public function timeBefore($field, $value, $parameters){
+    use ValidatesAttributes;
+    use ReplacesAttributes;
+
+    public function timeBefore($field, $value, array $parameters, $instance){
         $this->requireParameterCount(1, $parameters, 'TimeBefore');
-        $other = Arr::get($this->data, $parameters[0]);
 
-        $value = strtotime($value);
-        $other = strtotime($other);
-        
-        return $value < $other;
-    }
+        if(count($parameters) == 2){
+            $times = Arr::get($instance->getData(), $parameters[0]);
+            
+            $before = $value;
+            $before = strtotime(converterDataIngles($before));
 
-    public function timeAfter($field, $value, $parameters){
-        $this->requireParameterCount(1, $parameters, 'TimeAfter');
-        $other = Arr::get($this->data, $parameters[0]);
+            foreach($times as $time) {
+                $after  = $time[$parameters[1]];
+                $after  = strtotime(converterDataIngles($after));
 
-        $value = strtotime($value);
-        $other = strtotime($other);
-        
-        return $value > $other;
+                if($before > $after) {
+                    return false;
+                }
+            }
+        } else {
+            $after = Arr::get($instance->getData(), $parameters[0]);
+
+            if(is_null($after)){
+                throw new InvalidArgumentException('Validation rule TimeBefore requires at least 2 parameters on arrays.');
+            }
+
+            $before = strtotime(converterDataIngles($value));
+            $after  = strtotime(converterDataIngles($after));
+
+            return $before <= $after;
+        }
+
+        return true;
     }
 
     public function sex($field, $value){
