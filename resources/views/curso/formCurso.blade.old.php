132c54b6d50f272fbffb3d6b637cc5b68cd176db
Alex <asasouza.alex@gmail.com>
2017-05-08 22:43:37 -0300

    ...

--- resources/views/curso/formCurso.blade.old.php
+++ resources/views/curso/formCurso.blade.old.php
@@ -25,22 +25,18 @@
 		{!! Form::select('turno_id', $turnos, null, ['placeholder' => 'Escolha um turno', 'required', 'id' => 'turno_id', 'class' => 'form-control']) !!}
 	</div>
 
-	<div class="col-lg-8 form-group padding-left-0">
+	<div class="form-group">
 		{!! Form::label('funcionario', 'Coordenador', ['class' => 'control-label']) !!}
 		<a href="#" data-toggle="tooltip" data-placement='right' title="Caso o funcionário desejado não apareça na lista verifique se o mesmo está cadastrado no menu 'Funcionários' ou se o referido funcionário já não está cadastrado como coordenador de outro curso."><span class="glyphicon glyphicon-info-sign"></span></a>
 		{!! Form::select('funcionario_id', $funcionarios, null, ['placeholder' => 'Escolha um coordenador', 'id' => 'funcionario_id', 'class' => 'form-control']) !!}
 	</div>
 
-	<div class="col-lg-4 form-group padding-right-0">
-		{!! Form::label('modulos', 'Quantidade de Semestres', ['class' => 'control-label']) !!}
-		{!! Form::number('modulos', null, ['required', 'class' => 'form-control']) !!}
+	<div class="form-group">
+		{!! Form::label('disciplinas', 'Disciplinas cadastradas', ['class' => 'control-label col-xs-6 col-sm- 6 col-md-6 col-lg-6 padding-left-0']) !!}
+		{!! Form::label('disciplinas', 'Disciplinas selecionadas', ['class' => 'control-label col-xs-6 col-sm- 6 col-md-6 col-lg-6 padding-right-0', 'style' => 'padding-left: 5%;']) !!}
+		{!! Form::select('disciplina_id[]', $disciplinas, @$disciplina_id, 
+		['id' => 'disciplina_id', 'class' => 'form-control', 'multiple']) !!}
 	</div>
-
-	<div class="panel panel-default">
-		<div class="panel-heading"></div>
-		<div class="panel-body"></div>
-	</div>
-
 
 	<button type="submit" class="btn btn-success right"><span class="glyphicon glyphicon-floppy-disk"></span> Salvar</button>
 	<a class="btn btn-danger right cancelar" href="{{ route('cursos') }}"><span class="glyphicon glyphicon-remove"></span> Cancelar</a>
