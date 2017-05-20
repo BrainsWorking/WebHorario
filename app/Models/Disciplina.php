00e4beb5cc18ccc1f96745fe4bad0882f23c0c60
Lucas Leonel <Lucas Leonel>
2017-05-20 19:40:36 -0300

    Controller de FPA feito

--- app/Models/Disciplina.php
+++ app/Models/Disciplina.php
@@ -16,21 +16,7 @@ class Disciplina extends Model {
         return $this->belongsTo(Modulo::class);
     }
     
-<<<<<<< HEAD
-    public function modulo(){
-        return $this->belongsTo(Modulo::class);
-=======
     public function semestres(){
         return belongsToMany(Semestre::class, 'turmas')->withPivot('quantidade_alunos');
->>>>>>> c64fb49b2ade63cf217fc7d3d1f895ec7e1a771b
     }
-
-    public function fpas(){
-        return belongsToMany(Fpa::class, 'disciplinas_fpas')->withPivot('prioridade');
-    }
-
-    public function curso(){
-        return $this->modulo->curso;
-    }
-
 }
--- app/Models/Modulo.php
+++ app/Models/Modulo.php
@@ -3,13 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
-
-class Modulo extends Model {
-
-    protected $fillable = ['sigla'];
-	public $timestamps = false;	
-=======
 use App\Models\Curso;
 use App\Models\Modulo;
 use App\Models\Disciplina;
@@ -19,13 +12,10 @@ class Modulo extends Model {
     protected $table = 'modulos';
     protected $fillable = ['sigla', 'curso_id'];
     public    $timestamp = false;
->>>>>>> c64fb49b2ade63cf217fc7d3d1f895ec7e1a771b
 
     public function curso(){
         return $this->belongsTo(Curso::class);
     }
-<<<<<<< HEAD
-=======
 
     public function semestres(){
         return $this->belongsToMany(Modulo::class, 'modulos_semestres');
@@ -35,5 +25,4 @@ class Modulo extends Model {
         return $this->hasMany(Disciplina::class);
     }
 
->>>>>>> c64fb49b2ade63cf217fc7d3d1f895ec7e1a771b
 }
