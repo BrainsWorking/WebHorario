663236ca521d0208a40938cf1c65726e5bd32d53
Alex <asasouza.alex@gmail.com>
2017-05-21 01:49:37 -0300

    adicionado navtabs

--- resources/views/fpa/atribuicao_disciplinas.blade.php
+++ resources/views/fpa/atribuicao_disciplinas.blade.php
@@ -33,47 +33,55 @@
 
 <div class="col-lg-12">
 	<div class="col-lg-9 padding-left-0">
-		<div class="atrb-modulos panel panel-default">
-			@for($x = 1; $x <= 3; $x++)
-			<div class="atrb-semestre col-lg-12">
-				<h4 class="text-center">{{$x}}° Semestre</h4>
-				@for($i = 0; $i < 5; $i++)
-				<div class="atrb-disciplina">
-					<p><b>Nome:</b> LOPI - Lógica de Programação</p>
-					<p><b>Aulas Semanais:</b> 4</p>
-					<label for="professores">Professor: </label>
-					<select name="professores">
-						<option selected>Escolha um professor</option>
-						<optgroup label="Interessados">
-							<option>Mario Tadashi Shimanuki</option>
-							<option>Mario Tadashi Shimanuki</option>
-						</optgroup>
-						<optgroup label="Outros">
-							<option>Mario Tadashi Shimanuki</option>
-							<option>Mario Tadashi Shimanuki</option>
-						</optgroup>
-					</select>
-					<label for="horarios">Horários: </label>
-					<select name="horarios">
-						<option selected></option>
-						<optgroup label="Quinta">
-							<option>19:00 - 19:55</option>
-							<option>19:00 - 19:55</option>
-						</optgroup>
-						<optgroup label="Sexta">
-							<option>19:00 - 19:55</option>
-							<option>19:00 - 19:55</option>
-						</optgroup>
-					</select>
-				</div>
+		<div class="atrb-modulos">
+			<ul class='nav nav-tabs'>
+				<li class="active"><a data-toggle="pill" href="#1semestre">1° Semestre</a></li>
+				<li><a data-toggle="pill" href="#2semestre">2° Semestre</a></li>
+				<li><a data-toggle="pill" href="#3semestre">3° Semestre</a></li>
+			</ul>
+			<div class="tab-content">
+				@for($x = 1; $x <= 3; $x++)
+				<div id="{{$x}}semestre" class="disciplinas tab-pane fade in active">
+					<div class="atrb-semestre col-lg-12">
+						@for($i = 0; $i < 5; $i++)
+						<div class="atrb-disciplina">
+							<p><b>Nome:</b> LOPI - Lógica de Programação</p>
+							<p><b>Aulas Semanais:</b> 4</p>
+							<label for="professores">Professor: </label>
+							<select name="professores">
+								<option selected>Escolha um professor</option>
+								<optgroup label="Interessados">
+									<option>Mario Tadashi Shimanuki</option>
+									<option>Mario Tadashi Shimanuki</option>
+								</optgroup>
+								<optgroup label="Outros">
+									<option>Mario Tadashi Shimanuki</option>
+									<option>Mario Tadashi Shimanuki</option>
+								</optgroup>
+							</select>
+							<label for="horarios">Horários: </label>
+							<select name="horarios">
+								<option selected></option>
+								<optgroup label="Quinta">
+									<option>19:00 - 19:55</option>
+									<option>19:00 - 19:55</option>
+								</optgroup>
+								<optgroup label="Sexta">
+									<option>19:00 - 19:55</option>
+									<option>19:00 - 19:55</option>
+								</optgroup>
+							</select>
+						</div>
+						@endfor
+					</div>		
+				</div>		
 				@endfor
 			</div>
-			@endfor
 		</div>
 	</div>
 
 	<div class="col-lg-3">
-		<div class="panel panel-default">
+		<div class="">
 			<h4>Carga Horária Professores</h4>
 			<div id="professores" >
 				<div class="professor">
