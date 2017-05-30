42c098aad94de26c81e04caa519367962ce781a1
Alex <asasouza.alex@gmail.com>
2017-05-29 22:02:08 -0300

    Alterada função para exclusão de módulo

--- public/js/cadastro_curso.js
+++ public/js/cadastro_curso.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
         var close = $(this).closest('.disciplinas');
         var index = close.attr('id');
         var i = close.attr('data-modulo');
+
+        console.log(index);
+        console.log(i);
         
         close.append(`
          <div class="disciplina">
@@ -46,11 +49,15 @@ $(document).ready(function(){
          `);
 
         $('.tab-content').append(`
-            <input type="hidden" name="modulo_novo[`+ i +`][nome]" value="`+i+`º Semestre" hidden/>
+            
             <div id="semestre`+ i +`" class="disciplinas tab-pane fade in active" data-modulo = `+i+`>
+                <input type="hidden" name="modulo_novo[`+ i +`][nome]" value="`+i+`º Semestre" hidden/>
                 <div class="control-group" style="margin-left: 15px;">
                     <button type="button" class="btn btn-success add-field">
                         <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
+                    </button>
+                    <button type="button" data-target="semestre`+ i +`" class="btn btn-default remove-semestre" style="float: right; margin-top: 10px;">
+                        <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> Remover este semestre
                     </button>
                 </div>
                 <div class="disciplina">
@@ -79,14 +86,28 @@ $(document).ready(function(){
         
         i++;
     });
-    
-    $('.modulos').on("click", "#remove-semestre", function(){
-        var referencia = $('#last').prev('li').children().attr('href');
-        $('#last').prev('li').remove();
+
+    $('.modulos').on("click", ".remove-semestre", function(){
+        var referencia = '#' + $(this).attr('data-target');
+        var link_referencia = 'a[href="'+referencia+'"]';
+        $(link_referencia).parent().remove();
         $(referencia).remove();
-
+        $('div[id^=semestre]').each(function(index, el) {
+            $(el).attr('id', 'semestre'+(index+1));
+            $(el).attr('data-modulo', (index+1));
+            $(el).find('input[name$="[nome]"]').attr('value', (index+1) + '° Semestre');
+            $(el).find('input[name$="[nome]"]').attr('name', 'modulo_novo['+(index+1) + '][nome]');
+            $(el).find('.remove-semestre').attr('data-target', 'semestre'+(index+1));
+            $(el).find('input[name$="[disciplinas][nome][]"]').attr('name', 'modulo_novo['+(index+1)+'][nome][]');
+            $(el).find('input[name$="[disciplinas][sigla][]"]').attr('name', 'modulo_novo['+(index+1)+'][disciplinas][sigla][]');
+            $(el).find('select[name$="[disciplinas][tipo_sala][]"]').attr('name', 'modulo_novo['+(index+1)+'][disciplinas][tipo_sala][]');
+            $(el).find('input[name$="[disciplinas][aulas_semanais][]"]').attr('name', 'modulo_novo['+(index+1)+'][disciplinas][aulas_semanais][]');
+        });
+        $('a[href^="#semestre"]').each(function(index, el) {
+            $(el).attr('href', '#semestre' + (index+1));
+            $(el).html((index+1) + '° Semestre');
+        });
         referencia = '#semestre' + (i-2);
-
         if ($('#last').prev('li').length >= 1) {
             $('.active').removeClass('in active');
             $('#last').prev('li').addClass('active');
@@ -98,7 +119,6 @@ $(document).ready(function(){
         }
         if (i > 1) {
             i--;
-        };  
+        };
     });
-
 });
--- resources/views/curso/formCurso.blade.php
+++ resources/views/curso/formCurso.blade.php
@@ -38,133 +38,137 @@
 	<div class="col-lg-12 modulos" style="padding: 0px; margin-bottom: 15px;">
 		<ul class='nav nav-tabs'>
 			@foreach($curso->modulos as $modulo)
-				@if($loop->first)
-					<li class="active"><a data-toggle="pill" href="#sem{{$modulo->id}}">{{$modulo->nome}}</a></li>	
-				@elseif($loop->last)
-					<li id="last"><a data-toggle="pill" href="#sem{{$modulo->id}}">{{$modulo->nome}}</a></li>
-				@else
-					<li><a data-toggle="pill" href="#sem{{$modulo->id}}">{{$modulo->nome}}</a></li>
-				@endif
+			@if($loop->first)
+			<li class="active"><a data-toggle="pill" href="#sem{{$modulo->id}}">{{$modulo->nome}}</a></li>	
+			@elseif($loop->last)
+			<li id="last"><a data-toggle="pill" href="#sem{{$modulo->id}}">{{$modulo->nome}}</a></li>
+			@else
+			<li><a data-toggle="pill" href="#sem{{$modulo->id}}">{{$modulo->nome}}</a></li>
+			@endif
 			@endforeach
 			<li><a id="add-semestre" class="btn"><span class="glyphicon glyphicon-plus"></span></a></li>
-			<li><a id="remove-semestre" class="btn"><span class="glyphicon glyphicon-minus"></span></a></li>
 		</ul>
 
 		<div class="tab-content">
 			@foreach($curso->modulos as $modulo)
-				@if($loop->first)
-					<div id="sem{{$modulo->id}}" class="disciplinas tab-pane fade in active" data-modulo="{{$modulo->id}}">
+			@if($loop->first)
+			<div id="sem{{$modulo->id}}" class="disciplinas tab-pane fade in active" data-modulo="{{$modulo->id}}">
 				@else
-					<div id="sem{{$modulo->id}}" class="disciplinas tab-pane fade" data-modulo="{{$modulo->id}}">
-				@endif					
-						<input type="hidden" name="modulo[{{$modulo->id}}][id]" value="{{$modulo->id}}" hidden/>
-						<input type="hidden" name="modulo[{{$modulo->id}}][nome]" value="{{$modulo->nome}}" hidden/>
-						<div class="control-group" style="margin-left: 15px;">
-							<button type="button" class="btn btn-success add-field">
-								<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
-							</button>
+				<div id="sem{{$modulo->id}}" class="disciplinas tab-pane fade" data-modulo="{{$modulo->id}}">
+					@endif					
+					<input type="hidden" name="modulo[{{$modulo->id}}][id]" value="{{$modulo->id}}" hidden/>
+					<input type="hidden" name="modulo[{{$modulo->id}}][nome]" value="{{$modulo->nome}}" hidden/>
+					<div class="control-group" style="margin-left: 15px;">
+						<button type="button" class="btn btn-success add-field">
+							<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
+						</button>
+						<button type="button" data-target="sem{{$modulo->id}}" class="btn btn-default remove-semestre" style="float: right">
+							<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Remover este semestre
+						</button>
+					</div>
+					@foreach($modulo->disciplinas as $disciplina)
+					<div class="disciplina">
+						<div class="control-group form-group col-sm-5">
+							{!! Form::label('nome', 'Nome', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo[$modulo->id][disciplinas][nome][$disciplina->id]", $disciplina->nome, ['class' => 'form-control', 'required']) !!}
 						</div>
-						@foreach($modulo->disciplinas as $disciplina)
-							<div class="disciplina">
-								<div class="control-group form-group col-sm-5">
-									{!! Form::label('nome', 'Nome', ['class' => 'control-label']) !!}
-									{!! Form::text("modulo[$modulo->id][disciplinas][nome][$disciplina->id]", $disciplina->nome, ['class' => 'form-control', 'required']) !!}
-								</div>
-								<div class="control-group form-group col-sm-2">
-									{!! Form::label('sigla', 'Sigla', ['class' => 'control-label']) !!}
-									{!! Form::text("modulo[$modulo->id][disciplinas][sigla][$disciplina->id]", $disciplina->sigla, ['class' => 'form-control','maxlength' => '5', 'required']) !!}
-								</div>
-								<div class="control-group form-group col-sm-2">
-									{!! Form::label('tipoSala', 'Tipo Sala', ['class' => 'control-label']) !!}
-									{!! Form::select("modulo[$modulo->id][disciplinas][tipo_sala][$disciplina->id]", ['1' => 'Sala Comum', '2' => 'Laboratório de Informática'], null, ['class' => 'form-control']) !!}
-								</div>              
-								<div class="control-group form-group col-sm-2">
-									{!! Form::label('aulas_semanais', 'Aulas/Semana', ['class' => 'control-label']) !!}
-									{!! Form::text("modulo[$modulo->id][disciplinas][aulas_semanais][$disciplina->id]", $disciplina->aulas_semanais, ['class' => 'form-control', 'required']) !!}
-								</div>	
-							</div>
-						@endforeach
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('sigla', 'Sigla', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo[$modulo->id][disciplinas][sigla][$disciplina->id]", $disciplina->sigla, ['class' => 'form-control','maxlength' => '5', 'required']) !!}
+						</div>
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('tipoSala', 'Tipo Sala', ['class' => 'control-label']) !!}
+							{!! Form::select("modulo[$modulo->id][disciplinas][tipo_sala][$disciplina->id]", ['1' => 'Sala Comum', '2' => 'Laboratório de Informática'], null, ['class' => 'form-control']) !!}
+						</div>              
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('aulas_semanais', 'Aulas/Semana', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo[$modulo->id][disciplinas][aulas_semanais][$disciplina->id]", $disciplina->aulas_semanais, ['class' => 'form-control', 'required']) !!}
+						</div>	
 					</div>
-			@endforeach
-		</div>
-	</div>
-	@else
-	<div class="col-lg-12 modulos" style="padding: 0px; margin-bottom: 15px;">
-		<ul class='nav nav-tabs'>
-			<li class="active"><a data-toggle="pill" href="#semestre1">1° Semestre</a></li>
-			<li id="last"><a data-toggle="pill" href="#dp">DP</a></li>
-			<li><a id="add-semestre" class="btn"><span class="glyphicon glyphicon-plus"></span></a></li>
-			<li><a id="remove-semestre" class="btn"><span class="glyphicon glyphicon-minus"></span></a></li>
-		</ul>
-		<div class="tab-content">
-			<div id="semestre1" class="disciplinas tab-pane fade in active" data-modulo="1">
-				<input type="hidden" name="modulo_novo[1][nome]" value="1º Semestre" hidden/>
-				<div class="control-group" style="margin-left: 15px;">
-					<button type="button" class="btn btn-success add-field">
-						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
-					</button>
+					@endforeach
 				</div>
-				<div class="disciplina">
-					<div class="control-group form-group col-sm-5">
-						{!! Form::label('nome', 'Nome', ['class' => 'control-label']) !!}
-						{!! Form::text("modulo_novo[1][disciplinas][nome][]", null, ['class' => 'form-control', 'required']) !!}
-					</div>
-					<div class="control-group form-group col-sm-2">
-						{!! Form::label('sigla', 'Sigla', ['class' => 'control-label']) !!}
-						{!! Form::text("modulo_novo[1][disciplinas][sigla][]", null, ['class' => 'form-control','maxlength' => '5', 'required']) !!}
-					</div>
-					<div class="control-group form-group col-sm-2">
-						{!! Form::label('tipoSala', 'Tipo Sala', ['class' => 'control-label']) !!}
-						{!! Form::select("modulo_novo[1][disciplinas][tipo_sala][]", ['1' => 'Sala Comum', '2' => 'Laboratório de Informática'], null, ['class' => 'form-control']) !!}
-					</div>              
-					<div class="control-group form-group col-sm-2">
-						{!! Form::label('aulas_semanais', 'Aulas/Semana', ['class' => 'control-label']) !!}
-						{!! Form::text("modulo_novo[1][disciplinas][aulas_semanais][]", null, ['class' => 'form-control', 'required']) !!}
-					</div>
-				</div>
+				@endforeach
 			</div>
+		</div>
+		@else
+		<div class="col-lg-12 modulos" style="padding: 0px; margin-bottom: 15px;">
+			<ul class='nav nav-tabs'>
+				<li class="active"><a data-toggle="pill" href="#semestre1">1° Semestre</a></li>
+				<li id="last"><a data-toggle="pill" href="#dp">DP</a></li>
+				<li><a id="add-semestre" class="btn"><span class="glyphicon glyphicon-plus"></span></a></li>
+			</ul>
+			<div class="tab-content">
+				<div id="semestre1" class="disciplinas tab-pane fade in active" data-modulo="1">
+					<input type="hidden" name="modulo_novo[1][nome]" value="1º Semestre" hidden/>
+					<div class="control-group" style="margin-left: 15px;">
+						<button type="button" class="btn btn-success add-field">
+							<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
+						</button>
+						<button type="button" data-target='semestre1' class="btn btn-default remove-semestre" style="float: right; margin-top: 10px;">
+							<span class="glyphicon glyphicon-minus" aria-hidden="true"></span> Remover este semestre
+						</button>
+					</div>
+					<div class="disciplina">
+						<div class="control-group form-group col-sm-5">
+							{!! Form::label('nome', 'Nome', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo_novo[1][disciplinas][nome][]", null, ['class' => 'form-control', 'required']) !!}
+						</div>
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('sigla', 'Sigla', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo_novo[1][disciplinas][sigla][]", null, ['class' => 'form-control','maxlength' => '5', 'required']) !!}
+						</div>
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('tipoSala', 'Tipo Sala', ['class' => 'control-label']) !!}
+							{!! Form::select("modulo_novo[1][disciplinas][tipo_sala][]", ['1' => 'Sala Comum', '2' => 'Laboratório de Informática'], null, ['class' => 'form-control']) !!}
+						</div>              
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('aulas_semanais', 'Aulas/Semana', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo_novo[1][disciplinas][aulas_semanais][]", null, ['class' => 'form-control', 'required']) !!}
+						</div>
+					</div>
+				</div>
 
-			<div id="dp" class="disciplinas tab-pane fade" data-modulo="0">
-				<input type="hidden" name="modulo_novo[0][nome]" value="DP" hidden/>
-				<div class="control-group" style="margin-left: 15px;">
-					<button type="button" class="btn btn-success add-field">
-						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
-					</button>
-				</div>
-				<div class="disciplina">
-					<div class="control-group form-group col-sm-5">
-						{!! Form::label('nome', 'Nome', ['class' => 'control-label']) !!}
-						{!! Form::text("modulo_novo[0][disciplinas][nome][]", null, ['class' => 'form-control', 'required']) !!}
+				<div id="dp" class="disciplinas tab-pane fade" data-modulo="0">
+					<input type="hidden" name="modulo_novo[0][nome]" value="DP" hidden/>
+					<div class="control-group" style="margin-left: 15px;">
+						<button type="button" class="btn btn-success add-field">
+							<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar outras disciplinas
+						</button>
 					</div>
-					<div class="control-group form-group col-sm-2">
-						{!! Form::label('sigla', 'Sigla', ['class' => 'control-label']) !!}
-						{!! Form::text("modulo_novo[0][disciplinas][sigla][]", null, ['class' => 'form-control','maxlength' => '5', 'required']) !!}
-					</div>
-					<div class="control-group form-group col-sm-2">
-						{!! Form::label('tipoSala', 'Tipo Sala', ['class' => 'control-label']) !!}
-						{!! Form::select("modulo_novo[0][disciplinas][tipo_sala][]", ['1' => 'Sala Comum', '2' => 'Laboratório de Informática'], null, ['class' => 'form-control']) !!}
-					</div>              
-					<div class="control-group form-group col-sm-2">
-						{!! Form::label('aulas_semanais', 'Aulas/Semana', ['class' => 'control-label']) !!}
-						{!! Form::text("modulo_novo[0][disciplinas][aulas_semanais][]", null, ['class' => 'form-control', 'required']) !!}
+					<div class="disciplina">
+						<div class="control-group form-group col-sm-5">
+							{!! Form::label('nome', 'Nome', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo_novo[0][disciplinas][nome][]", null, ['class' => 'form-control', 'required']) !!}
+						</div>
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('sigla', 'Sigla', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo_novo[0][disciplinas][sigla][]", null, ['class' => 'form-control','maxlength' => '5', 'required']) !!}
+						</div>
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('tipoSala', 'Tipo Sala', ['class' => 'control-label']) !!}
+							{!! Form::select("modulo_novo[0][disciplinas][tipo_sala][]", ['1' => 'Sala Comum', '2' => 'Laboratório de Informática'], null, ['class' => 'form-control']) !!}
+						</div>              
+						<div class="control-group form-group col-sm-2">
+							{!! Form::label('aulas_semanais', 'Aulas/Semana', ['class' => 'control-label']) !!}
+							{!! Form::text("modulo_novo[0][disciplinas][aulas_semanais][]", null, ['class' => 'form-control', 'required']) !!}
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	@endif
+		@endif
 		<button type="submit" class="btn btn-success right"><span class="glyphicon glyphicon-floppy-disk"></span> Salvar</button>
 		<a class="btn btn-danger right cancelar" href="{{ route('cursos') }}"><span class="glyphicon glyphicon-remove"></span> Cancelar</a>
 
-	{!! Form::close() !!}
+		{!! Form::close() !!}
 
-	@endsection
+		@endsection
 
-	@section('scripts')
-	<script>
-	var i = {{ isset($curso) ? $curso->modulos->count() : '2' }};
-	$('#disciplina_id').multiSelect();
-	</script>
-	<script type="text/javascript" src="{{ asset('/js/confirmar-delete.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('/js/cadastro_curso.js') }}"></script>
-	@endsection
+		@section('scripts')
+		<script>
+		var i = {{ isset($curso) ? $curso->modulos->count() : '2' }};
+		$('#disciplina_id').multiSelect();
+		</script>
+		<script type="text/javascript" src="{{ asset('/js/confirmar-delete.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('/js/cadastro_curso.js') }}"></script>
+		@endsection
